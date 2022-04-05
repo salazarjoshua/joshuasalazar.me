6c99dcf3070f164ce02293d9bb92807ee7cf222e
Joshua Salazar <contact.joshuasalazar@gmail.com>
2022-04-05 23:12:21 +0800

    filter by member name and group

--- collectbook.js
+++ collectbook.js
@@ -1,182 +1,10 @@
 const photocards =
   [
-    // THE STORY BEGINS
-    {
-      "group": "TWICE",
-      "album": "The Story Begins",
-      "member": "Nayeon",
-      "img": "/assets/photocards/The Story Begins/tsb-random-01-nayeon.jpg",
-      "category": "have"
-    },
-    {
-      "group": "TWICE",
-      "album": "The Story Begins",
-      "member": "Tzuyu",
-      "img": "/assets/photocards/The Story Begins/tsb-random-09-tzuyu.jpg",
-      "category": "have"
-    },
-    {
-      "group": "TWICE",
-      "album": "The Story Begins",
-      "member": "Unit",
-      "img": "/assets/photocards/The Story Begins/tsb-unit-00-twice.jpg",
-      "category": "have"
-    },
-    //PAGE TWO
-    {
-      "group": "TWICE",
-      "album": "Page Two",
-      "member": "Dahyun",
-      "img": "/assets/photocards/Page Two/pt-lenti-07-dahyun.jpg",
-      "category": "have"
-    },
-    {
-      "group": "TWICE",
-      "album": "Page Two",
-      "member": "Jihyo",
-      "img": "/assets/photocards/Page Two/pt-random-05-jihyo.jpg",
-      "category": "have"
-    },
-    {
-      "group": "TWICE",
-      "album": "Page Two",
-      "member": "Dahyun",
-      "img": "/assets/photocards/Page Two/pt-random-07-dahyun.jpg",
-      "category": "have"
-    },
-    {
-      "group": "TWICE",
-      "album": "Page Two",
-      "member": "Unit",
-      "img": "/assets/photocards/Page Two/pt-unit-00-twice.jpg",
-      "category": "have"
-    },
-    // FANCY YOU
-    {
-      "group": "TWICE",
-      "album": "Fancy You",
-      "member": "Chaeyoung",
-      "img": "/assets/photocards/Fancy You/fy-mono-08-chaeyoung.jpg",
-      "category": "have"
-    },
-    // TASTE OF LOVE
-    {
-      "group": "TWICE",
-      "album": "Taste of Love",
-      "member": "Chaeyoung",
-      "img": "/assets/photocards/Taste of Love/tol-lenti-08-chaeyoung.jpg",
-      "category": "have"
-    },
-    {
-      "group": "TWICE",
-      "album": "Taste of Love",
-      "member": "All Members",
-      "img": "/assets/photocards/Taste of Love/tol-pob-00-twice.jpg",
-      "category": "have"
-    },
-    {
-      "group": "TWICE",
-      "album": "Taste of Love",
-      "member": "Nayeon",
-      "img": "/assets/photocards/Taste of Love/tol-pob-01-nayeon.jpg",
-      "category": "have"
-    },
-    {
-      "group": "TWICE",
-      "album": "Taste of Love",
-      "member": "Jeongyeon",
-      "img": "/assets/photocards/Taste of Love/tol-pob-02-jeongyeon.jpg",
-      "category": "have"
-    },
-    {
-      "group": "TWICE",
-      "album": "Taste of Love",
-      "member": "Momo",
-      "img": "/assets/photocards/Taste of Love/tol-pob-03-momo.jpg",
-      "category": "have"
-    },
-    {
-      "group": "TWICE",
-      "album": "Taste of Love",
-      "member": "Sana",
-      "img": "/assets/photocards/Taste of Love/tol-pob-04-sana.jpg",
-      "category": "have"
-    },
-    {
-      "group": "TWICE",
-      "album": "Taste of Love",
-      "member": "Jihyo",
-      "img": "/assets/photocards/Taste of Love/tol-pob-05-jihyo.jpg",
-      "category": "have"
-    },
-    {
-      "group": "TWICE",
-      "album": "Taste of Love",
-      "member": "Mina",
-      "img": "/assets/photocards/Taste of Love/tol-pob-06-mina.jpg",
-      "category": "have"
-    },
-    {
-      "group": "TWICE",
-      "album": "Taste of Love",
-      "member": "Dahyun",
-      "img": "/assets/photocards/Taste of Love/tol-pob-07-dahyun.jpg",
-      "category": "have"
-    },
-    {
-      "group": "TWICE",
-      "album": "Taste of Love",
-      "member": "Chaeyoung",
-      "img": "/assets/photocards/Taste of Love/tol-pob-08-chaeyoung.jpg",
-      "category": "have"
-    },
-    {
-      "group": "TWICE",
-      "album": "Taste of Love",
-      "member": "Tzuyu",
-      "img": "/assets/photocards/Taste of Love/tol-pob-09-tzuyu.jpg",
-      "category": "have"
-    },
-    {
-      "group": "TWICE",
-      "album": "Taste of Love",
-      "member": "Nayeon",
-      "img": "/assets/photocards/Taste of Love/tol-random-01-nayeon-2.jpg",
-      "category": "have"
-    },
-    {
-      "group": "TWICE",
-      "album": "Taste of Love",
-      "member": "Nayeon",
-      "img": "/assets/photocards/Taste of Love/tol-random-01-nayeon.jpg",
-      "category": "have"
-    },
-    {
-      "group": "TWICE",
-      "album": "Taste of Love",
-      "member": "Sana",
-      "img": "/assets/photocards/Taste of Love/tol-random-04-sana-2.jpg",
-      "category": "have"
-    },
-    {
-      "group": "TWICE",
-      "album": "Taste of Love",
-      "member": "Sana",
-      "img": "/assets/photocards/Taste of Love/tol-random-04-sana.jpg",
-      "category": "have"
-    },
-    {
-      "group": "TWICE",
-      "album": "Taste of Love",
-      "member": "Chaeyoung",
-      "img": "/assets/photocards/Taste of Love/tol-random-08-chaeyoung.jpg",
-      "category": "have"
-    },
     // FORMULA OF LOVE
     {
       "group": "TWICE",
       "album": "Formula of Love",
-      "member": "TWICE",
+      "member": "All Members (TWICE)",
       "img": "/assets/photocards/Formula of Love/fol-pob-00-twice.jpg",
       "category": "have"
     },
@@ -300,14 +128,185 @@ const photocards =
       "category": "want"
     },
     {
-      "group": "RV",
+      "group": "Red Velvet",
       "album": "Formula of Love",
       "member": "Sana",
       "img": "/assets/photocards/Formula of Love/fol-scratch-04-sana.jpg",
       "category": "want"
+    },
+    // TASTE OF LOVE
+    {
+      "group": "TWICE",
+      "album": "Taste of Love",
+      "member": "Chaeyoung",
+      "img": "/assets/photocards/Taste of Love/tol-lenti-08-chaeyoung.jpg",
+      "category": "have"
+    },
+    {
+      "group": "TWICE",
+      "album": "Taste of Love",
+      "member": "All Members (TWICE)",
+      "img": "/assets/photocards/Taste of Love/tol-pob-00-twice.jpg",
+      "category": "have"
+    },
+    {
+      "group": "TWICE",
+      "album": "Taste of Love",
+      "member": "Nayeon",
+      "img": "/assets/photocards/Taste of Love/tol-pob-01-nayeon.jpg",
+      "category": "have"
+    },
+    {
+      "group": "TWICE",
+      "album": "Taste of Love",
+      "member": "Jeongyeon",
+      "img": "/assets/photocards/Taste of Love/tol-pob-02-jeongyeon.jpg",
+      "category": "have"
+    },
+    {
+      "group": "TWICE",
+      "album": "Taste of Love",
+      "member": "Momo",
+      "img": "/assets/photocards/Taste of Love/tol-pob-03-momo.jpg",
+      "category": "have"
+    },
+    {
+      "group": "TWICE",
+      "album": "Taste of Love",
+      "member": "Sana",
+      "img": "/assets/photocards/Taste of Love/tol-pob-04-sana.jpg",
+      "category": "have"
+    },
+    {
+      "group": "TWICE",
+      "album": "Taste of Love",
+      "member": "Jihyo",
+      "img": "/assets/photocards/Taste of Love/tol-pob-05-jihyo.jpg",
+      "category": "have"
+    },
+    {
+      "group": "TWICE",
+      "album": "Taste of Love",
+      "member": "Mina",
+      "img": "/assets/photocards/Taste of Love/tol-pob-06-mina.jpg",
+      "category": "have"
+    },
+    {
+      "group": "TWICE",
+      "album": "Taste of Love",
+      "member": "Dahyun",
+      "img": "/assets/photocards/Taste of Love/tol-pob-07-dahyun.jpg",
+      "category": "have"
+    },
+    {
+      "group": "TWICE",
+      "album": "Taste of Love",
+      "member": "Chaeyoung",
+      "img": "/assets/photocards/Taste of Love/tol-pob-08-chaeyoung.jpg",
+      "category": "have"
+    },
+    {
+      "group": "TWICE",
+      "album": "Taste of Love",
+      "member": "Tzuyu",
+      "img": "/assets/photocards/Taste of Love/tol-pob-09-tzuyu.jpg",
+      "category": "have"
+    },
+    {
+      "group": "TWICE",
+      "album": "Taste of Love",
+      "member": "Nayeon",
+      "img": "/assets/photocards/Taste of Love/tol-random-01-nayeon-2.jpg",
+      "category": "have"
+    },
+    {
+      "group": "TWICE",
+      "album": "Taste of Love",
+      "member": "Nayeon",
+      "img": "/assets/photocards/Taste of Love/tol-random-01-nayeon.jpg",
+      "category": "have"
+    },
+    {
+      "group": "TWICE",
+      "album": "Taste of Love",
+      "member": "Sana",
+      "img": "/assets/photocards/Taste of Love/tol-random-04-sana-2.jpg",
+      "category": "have"
+    },
+    {
+      "group": "TWICE",
+      "album": "Taste of Love",
+      "member": "Sana",
+      "img": "/assets/photocards/Taste of Love/tol-random-04-sana.jpg",
+      "category": "have"
+    },
+    {
+      "group": "TWICE",
+      "album": "Taste of Love",
+      "member": "Chaeyoung",
+      "img": "/assets/photocards/Taste of Love/tol-random-08-chaeyoung.jpg",
+      "category": "have"
+    },
+    // FANCY YOU
+    {
+      "group": "TWICE",
+      "album": "Fancy You",
+      "member": "Chaeyoung",
+      "img": "/assets/photocards/Fancy You/fy-mono-08-chaeyoung.jpg",
+      "category": "have"
+    },
+    //PAGE TWO
+    {
+      "group": "TWICE",
+      "album": "Page Two",
+      "member": "Dahyun",
+      "img": "/assets/photocards/Page Two/pt-lenti-07-dahyun.jpg",
+      "category": "have"
+    },
+    {
+      "group": "TWICE",
+      "album": "Page Two",
+      "member": "Jihyo",
+      "img": "/assets/photocards/Page Two/pt-random-05-jihyo.jpg",
+      "category": "have"
+    },
+    {
+      "group": "TWICE",
+      "album": "Page Two",
+      "member": "Dahyun",
+      "img": "/assets/photocards/Page Two/pt-random-07-dahyun.jpg",
+      "category": "have"
+    },
+    {
+      "group": "TWICE",
+      "album": "Page Two",
+      "member": "Unit (TWICE)",
+      "img": "/assets/photocards/Page Two/pt-unit-00-twice.jpg",
+      "category": "have"
+    },
+    // THE STORY BEGINS
+    {
+      "group": "TWICE",
+      "album": "The Story Begins",
+      "member": "Nayeon",
+      "img": "/assets/photocards/The Story Begins/tsb-random-01-nayeon.jpg",
+      "category": "have"
+    },
+    {
+      "group": "TWICE",
+      "album": "The Story Begins",
+      "member": "Tzuyu",
+      "img": "/assets/photocards/The Story Begins/tsb-random-09-tzuyu.jpg",
+      "category": "have"
+    },
+    {
+      "group": "TWICE",
+      "album": "The Story Begins",
+      "member": "Unit (TWICE)",
+      "img": "/assets/photocards/The Story Begins/tsb-unit-00-twice.jpg",
+      "category": "have"
     }
-  ];
-
+  ]
 
 
 // Populate Dropdown
@@ -317,66 +316,93 @@ function populateDropdown(dropdown, key) {
   for (var i = 0; i < unique.length; i++) {
     var data = unique[i];
     document.createElement("button");
-    var dropdownIndex = `<button value="${data}" data-filter="${key}">${data}</button>\n`
+    var dropdownIndex = `<li><button value="${data}" data-filter="${key}">${data}</button></li>\n`
     dropdown.innerHTML += dropdownIndex;
   }
 }
 
-const categoryButtons = Array.from(document.querySelectorAll('[name="categories"]'));
 const groupButtonContainers = document.querySelector('#groups');
 const memberButtonContainers = document.querySelector('#members');
-
 populateDropdown(groupButtonContainers, "group");
 populateDropdown(memberButtonContainers, "member");
 
 
 var groupButtons = Array.from(document.querySelectorAll('#groups button'));
-
 var memberButtons = Array.from(document.querySelectorAll('#members button'));
+const categoryButtons = Array.from(document.querySelectorAll('.btn--category'));
 
 
+var dropdownButtons = document.querySelectorAll('.btn--dropdown');
+var dropdownListButtons = document.querySelectorAll('.dropdown__list button');
 
-// Filter Photocards
-// memberDropDown.value = filters.member;
-// groupDropDown.value = filters.group;
-// categoryButtons.value = filters.category;
+// Show Dropdown
+function handleDropDown() {
+  const nextElement = this.nextElementSibling;
+  nextElement.classList.toggle('collapse');
+}
+
+function handleDropDownList() {
+  const dropdown = this.closest(".dropdown");
+  const dropdownButton = dropdown.querySelector(".btn--dropdown");
+  const dropdownButtonSpan = dropdown.querySelector("button span");
+  dropdownButtonSpan.innerHTML = this.innerHTML;
+  dropdownButton.focus();
+}
+
+// Change Active State
+function changeActiveState() {
+  const dropdownList = this.closest(".dropdown__list");
+  const current = dropdownList.querySelector("button.selected");
+  current.classList.remove("selected");
+  this.classList.add("selected");
+  dropdownList.classList.add("collapse");
+}
+
+function changeSelectedState() {
+  if (document.querySelector('.btn--active') !== null) {
+    document.querySelector('.btn--active').classList.remove('btn--active');
+  }
+
+  this.classList.add('btn--active');
+}
+
+
+dropdownButtons.forEach((button) => button.addEventListener('click', handleDropDown));
+dropdownListButtons.forEach((button) => button.addEventListener('click', handleDropDownList));
+dropdownListButtons.forEach((button) => button.addEventListener('click', changeActiveState));
+categoryButtons.forEach((button) => button.addEventListener('click', changeSelectedState));
 
 // Filter by Photocards
+
 var filters = {
-  member: "",
-  group: "",
-  category: "have",
+  member: '',
+  group: '',
+  category: 'have',
 };
 
 function handleChangeFilter() {
   filters[this.dataset.filter] = this.value;
-  console.log(this.dataset.filter);
-  update();
-
+  updateFilter();
 }
 
-categoryButtons.forEach((input) => input.addEventListener('click', handleChangeFilter));
+categoryButtons.forEach((button) => button.addEventListener('click', handleChangeFilter));
 groupButtons.forEach((button) => button.addEventListener('click', handleChangeFilter));
 memberButtons.forEach((button) => button.addEventListener('click', handleChangeFilter));
 
 
-function filterMembers(photocard) {
-  return (!filters.member.length || photocard.member == filters.member);
-}
+function updateFilter() {
+  filteredPhotocards = photocards.filter(item => {
+    for (let key in filters) {
+      if (!(item[key] === undefined || item[key] == filters[key]) && !(filters[key].length == 0)) {
+        return false;
+      }
+    }
+    return true;
+  });
 
-function filterGroups(photocard) {
-  return !filters.group.length || photocard.group == filters.group;
-}
-
-function filterCategories(photocard) {
-  return !filters.category.length || photocard.category == filters.category
-}
-
-function update() {
-  let filteredCards = photocards.filter(filterMembers).filter(filterGroups).filter(filterCategories);
-
+  console.table(filters)
   photocardsContainer.innerHTML = ""
-  if (filteredCards.length == 0) {
+  if (filteredPhotocards.length == 0) {
     photocardsContainer.innerHTML = `
     <div style="text-align: center;">
     <img src="/assets/avatar-sad.svg" alt="" style="display: inline-block;">
@@ -388,7 +414,9 @@ function update() {
   const photocardsList = document.createElement('ul');
   photocardsList.classList.add('photocards');
   photocardsContainer.appendChild(photocardsList);
-  photocardsList.innerHTML = filteredCards.map(photocard => `<li><img src="${photocard.img}" alt="${photocard.group} - ${photocard.member}" class="photocard"></li>`).join("");
-};
+  photocardsList.innerHTML = filteredPhotocards.map(photocard => `<li><img src="${photocard.img}" alt="${photocard.group} - ${photocard.member}" class="photocard"></li>`).join("");
 
-update();
+}
+
+updateFilter();
+
